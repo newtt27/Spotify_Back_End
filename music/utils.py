d1896309bed739050237847e5f4888ec6c9f3273
Tan <moctan137@gmail.com>
2025-05-08 14:16:23 +0700

    Remove function

--- music/utils.py
+++ music/utils.py
@@ -4,3 +4,4 @@ def get_related_songs_by_genre(track, limit=5):
     track_genres =  Track.objects.filter(id=track.id).values_list('genres', flat=True) #Lấy tất cả các thể loại của bài hát hiện tại
     related_tracks = Track.objects.filter(genres__in=track_genres).exclude(id=track.id).distinct().order_by('?')[:limit] #Lấy ngẫu nhiên bài hát cùng thể loại với bài hát hiện tại
     return related_tracks
+
